df726897d69dd803500633d7c758885f432d2004
Victor Varza <vvarza@adobe.com>
2019-07-22 00:28:52 +0300

    add more labs

--- src/kubeadm/src/scripts/setup.sh
+++ src/kubeadm/src/scripts/setup.sh
@@ -11,7 +11,5 @@ LC_ALL=en_US.utf-8
 EOF
 
 # configure /etc/hosts file
-echo "192.168.101.100   master master.local" >> /etc/hosts
-echo "192.168.101.101   node01 node01.local" >> /etc/hosts
-echo "192.168.101.102   node02 node02.local" >> /etc/hosts
-echo "192.168.101.103   node03 node03.local" >> /etc/hosts
+echo "192.168.101.100   lab-master lab-master.local" >> /etc/hosts
+echo "192.168.101.101   lab-node01 lab-node01.local" >> /etc/hosts
