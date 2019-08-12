2954c097081bb3bf94b61b5d89ea757eb2ed36ad
Victor Varza <vvarza@adobe.com>
2019-08-12 14:38:23 +0300

    fix download boxes

--- src/init/download_boxes.sh
+++ src/init/download_boxes.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 UBUNTU_BOX_IMAGE="ubuntu/bionic64"
-UBUNTU_BOX_VERSION = "20190801.1.0"
+UBUNTU_BOX_VERSION="20190801.1.0"
 
 CENTOS_BOX_IMAGE="centos/7"
-CENTOS_BOX_VERSION = "1905.1"
+CENTOS_BOX_VERSION="1905.1"
 
 KUBERNETES_BOX_IMAGE="hlesey/k8s-base"
-KUBERNETES_BOX_VERSION = "1.15.1"
+KUBERNETES_BOX_VERSION="1.15.1"
 
 # add ubuntu
 vagrant box add $UBUNTU_BOX_IMAGE --box-version $UBUNTU_BOX_VERSION --provider virtualbox
