72ed639cd4add179b0fa8581c2631748aa3a883e
Victor Varza <victor.varza@gmail.com>
2021-03-20 22:30:52 +0200

    update kubeadm lab

--- src/kubeadm/src/scripts/setup.sh
+++ src/kubeadm/src/scripts/setup.sh
@@ -12,4 +12,4 @@ EOF
 
 # configure /etc/hosts file
 echo "192.168.235.100   lab-control-plane lab-control-plane.local" >> /etc/hosts
-echo "192.168.235.101   lab-node01 lab-node01.local" >> /etc/hosts
+echo "192.168.235.101   lab-node lab-node.local" >> /etc/hosts
